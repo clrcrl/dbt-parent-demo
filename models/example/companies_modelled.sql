91149af3c87983e07ccb1d3e1f1c9cb21a606d68
Claire Carroll <claire@Claires-MacBook-Air.local>
2018-03-05 20:51:15 +1100

    Add models that use macro

---
+++ models/example/companies_modelled.sql
@@ -0,0 +1,2 @@
+-- need to replace "analytics_claire" with target
+{{parent_details(table = "analytics_claire.companies", entity_id_column_name = "company_id", parent_id_column_name = "parent_company_id")}}
---
+++ models/example/family_tree_modelled.sql
@@ -0,0 +1,3 @@
+-- need to replace "analytics_claire" with target
+
+{{parent_details(table = "analytics_claire.male_family_tree", entity_id_column_name = "id", parent_id_column_name = "parent_id", detail_column_names = ["house", "patronus"])}}
