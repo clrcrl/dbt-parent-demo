1cd543d0df3269d4ed0ea0c49067b92f069ef604
Claire Carroll <carroll.claire.e@gmail.com>
2018-03-10 13:20:27 +1100

    Adding multiple options for this macro

--- models/example/companies_modelled_advanced.sql
+++ models/example/companies_modelled_advanced.sql
@@ -1,6 +1,8 @@
 -- need to replace "analytics_claire" with target
+-- come back and put some better examples in, such as calulating number of direct and total descendants, and rolled up revenue
 
 {{
-    parent_details_advanced(table = "analytics_claire.companies",
+    parent_details_create_level_tables(table = "analytics_claire.companies",
                             entity_id_column_name = "company_id",
                             parent_id_column_name = "parent_company_id")}}
+
--- models/example/family_tree_modelled.sql
+++ models/example/family_tree_modelled.sql
@@ -1,3 +1,32 @@
 -- need to replace "analytics_claire" with target
+/*
+-- option 0
+{{parent_details(
+    table = "analytics_claire.male_family_tree",
+    entity_id_column_name = "id",
+    parent_id_column_name = "parent_id")}}
 
-{{parent_details(table = "analytics_claire.male_family_tree", entity_id_column_name = "id", parent_id_column_name = "parent_id", detail_column_names = ["house", "patronus"])}}
+
+-- option 1
+    
+{{parent_details_create_level_tables(
+    table = "analytics_claire.male_family_tree",
+    entity_id_column_name = "id",
+    parent_id_column_name = "parent_id",
+    target_table = "family_tree_level")}}
+
+    
+-- option 2
+{{parent_details_insert_into_table(
+    table = "analytics_claire.male_family_tree",
+    entity_id_column_name = "id",
+    parent_id_column_name = "parent_id",
+    target_table = "family_tree_levels")}}
+*/  
+-- option 3
+{{parent_details_generate_sql(
+    table = "analytics_claire.male_family_tree",
+    entity_id_column_name = "id",
+    parent_id_column_name = "parent_id")}}
+/*
+*/
