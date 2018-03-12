ddcbfb9923311020e5a42881a26114884dbb1e59
Claire Carroll <carroll.claire.e@gmail.com>
2018-03-13 09:04:50 +1100

    Clean up for sharing

--- models/example/family_tree_modelled.sql
+++ models/example/family_tree_modelled.sql
@@ -1,31 +1,4 @@
-/*
--- option 0
 {{parent_details(
     table = "{{schema}}.male_family_tree",
     entity_id_column_name = "id",
     parent_id_column_name = "parent_id")}}
-
-
--- option 1
-    
-{{parent_details_create_level_tables(
-    table = "{{schema}}.male_family_tree",
-    entity_id_column_name = "id",
-    parent_id_column_name = "parent_id",
-    target_table = "family_tree_level")}}
-
-    
--- option 2
-{{parent_details_insert_into_table(
-    table = "{{schema}}.male_family_tree",
-    entity_id_column_name = "id",
-    parent_id_column_name = "parent_id",
-    target_table = "family_tree_levels")}}
-*/  
--- option 3
-{{parent_details_generate_sql(
-    table = "{{schema}}.male_family_tree",
-    entity_id_column_name = "id",
-    parent_id_column_name = "parent_id")}}
-/*
-*/
